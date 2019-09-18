8d9055129f432ddf8c8a3cb2581333e824b4b08c
HyunGyu-Lee <gusrb0808@naver.com>
2019-09-19 00:22:33 +0900

    공통코드 API 문서 추가

--- backend/src/main/java/com/hst/shrp/controller/CommonCodeController.java
+++ backend/src/main/java/com/hst/shrp/controller/CommonCodeController.java
@@ -3,6 +3,7 @@ package com.hst.shrp.controller;
 import com.hst.shrp.model.api.ApiResponse;
 import com.hst.shrp.model.api.code.CommonCodesResponse;
 import com.hst.shrp.service.CommonCodeService;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author dlgusrb0808@gmail.com
  */
+@Api( tags = "0. 공통코드 API", description = "공통코드 관련 기능을 제공합니다.")
 @RestController
 @RequestMapping("/api/codes")
 public class CommonCodeController {
@@ -19,12 +21,19 @@ public class CommonCodeController {
 	@Autowired
 	private CommonCodeService commonCodeService;
 
-	/***
-	 * GET /api/codes/{groupCode}
-	 *
-	 * @param groupCode the groupCode
-	 * @return
-	 */
+	@ApiOperation(value = "공통코드 조회", notes = "공통그룹코드 및 공통코드 목록를 조회합니다.")
+	@ApiImplicitParams({
+			@ApiImplicitParam(
+					name = "groupCode",
+					value = "공통그룹코드 (100, 200, ...)",
+					paramType = "path",
+					dataType = "string",
+					required = true
+			)
+	})
+	@ApiResponses({
+			@io.swagger.annotations.ApiResponse(code = 200, message = "성공")
+	})
 	@GetMapping("{groupCode}")
 	public ApiResponse<CommonCodesResponse> getCodes(@PathVariable String groupCode) {
 		CommonCodesResponse commonCodesResponse = commonCodeService.getCommonCodes(groupCode);
