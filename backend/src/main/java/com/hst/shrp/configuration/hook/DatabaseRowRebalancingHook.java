94bd1d8990d0e41cc7c8edd7a531245d5a46888b
HyunGyu-Lee <gusrb0808@naver.com>
2019-09-24 22:36:48 +0900

    시뮬레이션, 시뮬레이션 삭제 로직 추가

--- backend/src/main/java/com/hst/shrp/configuration/hook/DatabaseRowRebalancingHook.java
+++ backend/src/main/java/com/hst/shrp/configuration/hook/DatabaseRowRebalancingHook.java
@@ -3,8 +3,14 @@ package com.hst.shrp.configuration.hook;
 import com.hst.shrp.configuration.annotation.InitializingHook;
 import com.hst.shrp.service.AnalysisService;
 import com.hst.shrp.service.SimulationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
@@ -20,6 +26,9 @@ public class DatabaseRowRebalancingHook extends ApplicationInitializingHook {
 	@Value("${shrp.data.number-of-keey-recent-simulations}")
 	private int recentSimulationNumberLimitBound;
 
+	@Autowired
+	private PlatformTransactionManager transactionManager;	// commit control transaction manually
+
 	private final SimulationService simulationService;
 	private final AnalysisService analysisService;
 
@@ -29,22 +38,32 @@ public class DatabaseRowRebalancingHook extends ApplicationInitializingHook {
 	}
 
 	@Override
-	protected boolean isNecessaryExecuteHook() throws Exception {
+	protected boolean isNecessaryExecuteHook() {
 		List<Integer> deleteTargetSimulationNumbers = simulationService.getNonRecentlySimulationNumbers(recentSimulationNumberLimitBound);
 		setContextAttribute(DELETE_TARGET_SIMULATION_NUMBERS_ATTRIBUTE_NAME, deleteTargetSimulationNumbers);
 		return deleteTargetSimulationNumbers.size() != 0;
 	}
 
 	@Override
-	protected void executeHook() throws Exception {
+	protected void executeHook() {
 		List<Integer> deleteTargetSimulationNumbers = (List<Integer>) getContextAttribute(DELETE_TARGET_SIMULATION_NUMBERS_ATTRIBUTE_NAME);
 
 		logger.info("Need to delete old simulation {} rows", deleteTargetSimulationNumbers.size());
 
-		// delete old simulations
-		simulationService.removeNonRecentlySimulations(deleteTargetSimulationNumbers);
+		TransactionDefinition definition = new DefaultTransactionDefinition();
+		TransactionStatus status = transactionManager.getTransaction(definition);
 
-		// delete old simulation analysis histories
-		analysisService.removeOldSimulationAnalysisHistories(deleteTargetSimulationNumbers);
+		try {
+			// delete old simulations
+			simulationService.removeNonRecentlySimulations(deleteTargetSimulationNumbers);
+
+			// delete old simulation analysis histories
+			analysisService.removeOldSimulationAnalysisHistories(deleteTargetSimulationNumbers);
+		} catch (Exception e) {
+			transactionManager.rollback(status);
+			throw e;
+		}
+
+		transactionManager.commit(status);
 	}
 }
